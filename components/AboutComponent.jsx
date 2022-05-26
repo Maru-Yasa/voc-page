1ddfd504bc523b5e6c2a0c579f009fb3d1f45eff
maru-yasa <marufilyasa13@gmail.com>
2022-05-26 22:21:34 +0700

    make responsive in adnroid

--- components/AboutComponent.jsx
+++ components/AboutComponent.jsx
@@ -6,7 +6,7 @@ export default function AboutComponent(){
     
         <div className="row justify-content-center mt-5">
             
-            <div data-aos="fade-up" className="col-md-9 row justify-content-center my-bg my-card px-5 py-4">
+            <div data-aos="fade-up" className="col-10 col-md-9 row justify-content-center my-bg my-card px-5 py-4">
                 <h1 className="text-blue fw-bold"> <i className="bo bi-paperclip"></i> About</h1>
                 <div className="col-12 mt-3">
                     <p>
